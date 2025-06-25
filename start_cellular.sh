6909191039a31bcecd6dea66316835ff820791a4
hong.guo <hong.guo@advantech.com.cn>
2025-06-25 10:59:31 +0800

    start_cellular set a container name with cellular_guard

--- start_cellular.sh
+++ start_cellular.sh
@@ -2,7 +2,7 @@
 # 
 # This script is for testing cellular guard in BalenaOS.
 
-cellular_guard="balena run --rm -it --privileged --network host \
+cellular_guard="balena run --rm -it --privileged --network host --name cellular_guard \
 -v /run/dbus:/host/run/dbus -v /sys:/sys -v /proc:/proc -v /mnt/data:/mnt/data \
 -e DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket \
 cellular_guard"
